7c94f7f615bf6d649c079dae59e12e1b49c51430
karlla souzza <karlla.souzza7@gmail.com>
2022-08-09 18:40:42 -0300

    📚 Updating the tasks

--- src/controllers/Tarefas.js
+++ src/controllers/Tarefas.js
@@ -1,20 +1,25 @@
+/* Estamos importando nosso banco simulado e nosso model para
+ podermos usa-los neste arquivo */
 import TarefasM from "../models/Tarefas.js";
 import { bdTarefas } from "../infra/bd.js";
 
+//  Estamos exportando uma const
 export const Tarefas = (app) => {
   app.get("/tarefas", (req, res) => {
-    res.send(bdTarefas);
+    res.send(bdTarefas)
   });
 
   app.post("/tarefas", (req, res) => {
     const { idUsuario, texto, status } = req.body;
 
     const dataM = new TarefasM(idUsuario, texto, status);
-    bdTarefas.push(dataM)
+    bdTarefas.push(dataM);
     res.send(bdTarefas);
   });
 
-  app.get('/tarefas/:id', (req, res) => {
-    res.send(req.params.id)
-  })
+  app.get("/tarefas/:id", (req, res) => {
+    const data = bdTarefas.filter((element) => element.id === req.params.id);
+
+    res.send(data);
+  });
 };
